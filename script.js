932dbcbd3a50993012df2b1cd74ee8139b2206e4
scriptedd_ <oguzturan0706@gmail.com>
2025-06-25 13:39:38 +0300

    beta v0.5

---
+++ script.js
@@ -0,0 +1,15 @@
+const abt = document.querySelector(".abt");
+const cont = document.querySelector(".cont");
+const conf = document.querySelector(".conf");
+
+abt.addEventListener("click", () => {
+    alert("Better-Start-Menu v0.5")
+});
+
+cont.addEventListener("click", () => {
+    alert("You can't, sorry.")
+});
+
+conf.addEventListener("click", () => {
+    alert("You can change the Start Menu to your liking using the files given and configuring them.")
+});
